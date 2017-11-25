7a3fd1663612178012e6d22fd254b3b9c38a9738
yurahuna <yurahuna@gmail.com>
2017-11-25 17:41:48 +0900

    implement nanostl::pair in nanoutility.h

--- nanoutility.h
+++ nanoutility.h
@@ -27,12 +27,27 @@ THE SOFTWARE.
 
 namespace nanostl {
 
-template <class T1, class T2> struct pair;
+template <class T1, class T2>
+struct pair {
+  T1 first;
+  T2 second;
+  pair() {}
+  pair(const T1& a, const T2& b) : first(a), second(b) {}
+};
 
 template <class T1, class T2>
-pair<T1, T2> make_pair(T1 x, T2 y)
-{
-  return pair<T1, T2>(x, y);
+inline bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y) {
+    return x.first == y.first && x.second == y.second;
+}
+
+template <class T1, class T2>
+inline bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y) {
+    return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+}
+
+template <class T1, class T2>
+inline pair<T1, T2> make_pair(const T1& x, const T2& y) {
+    return pair<T1, T2>(x, y);
 }
 
 }  // namespace nanostl
