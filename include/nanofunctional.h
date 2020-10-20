6beb1e5df0ca6b31e17d15252b3b73841d390fdc
Syoyo Fujita <syoyo@lighttransport.com>
2020-10-21 03:04:12 +0900

    Add SipHash. Add variant-lite(porting W.I.P.)

--- include/nanofunctional.h
+++ include/nanofunctional.h
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
- * Copyright (c) 2017 Light Transport Entertainment, Inc.
- * 
+ *
+ * Copyright (c) 2020 Light Transport Entertainment, Inc.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,48 +22,19 @@
  * THE SOFTWARE.
  */
 
-#ifndef NANOSTL_UTILITY_H_
-#define NANOSTL_UTILITY_H_
+#ifndef NANOSTL_FUNCTIONAL_H_
+#define NANOSTL_FUNCTIONAL_H_
 
-#include "nanotype_traits.h"
+#ifdef NANOSTL_DEBUG
+#include <iostream>
+#endif
+
+#include "__hashfunc.h"
 
 namespace nanostl {
 
-template <class T1, class T2>
-struct pair {
-  T1 first;
-  T2 second;
-  pair() {}
-  pair(const T1& a, const T2& b) : first(a), second(b) {}
-};
-
-template <class T1, class T2>
-inline bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y) {
-  return x.first == y.first && x.second == y.second;
-}
-
-template <class T1, class T2>
-inline bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y) {
-  return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
-}
-
-template <class T1, class T2>
-inline pair<T1, T2> make_pair(const T1& x, const T2& y) {
-  return pair<T1, T2>(x, y);
-}
-
-// limited implementation of swap
-template <class T>
-inline void swap(T &a, T &b) noexcept
-{
-  T tmp;
-  tmp = a;
-  a = b;
-  b = tmp;
-}
-
-template <class T> typename add_rvalue_reference<T>::type declval() noexcept;
+template< class Key > struct hash;
 
 }  // namespace nanostl
 
-#endif  // NANOSTL_UTILITY_H_
+#endif  // NANOSTL_FUNCTIONAL_H_
---
+++ sandbox/hash/main-hash.cc
@@ -0,0 +1,8 @@
+#include "nanofunctional.h"
+
+int main(int argc, char **argv)
+{
+  nanostl::hash<int> a;
+
+  return 0;
+}
